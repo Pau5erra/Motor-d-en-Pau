b6165a9fe7c4bfd05e75148a1a37572b37ebcc89
Pau5erra <pau.serra11@hotmail.com>
2016-09-13 14:11:00 +0200

    be

---
+++ RaceGame/ModuleSceneIntro.cpp
@@ -0,0 +1,59 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+#include "ModulePlayer.h"
+#include "PhysVehicle3D.h"
+#include <ctime>
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+	
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+	
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,36 @@
+A 3D Race Game
+
+This game consist of to take 10 balls before 20 seconds!
+
+If you take all the balls, then your time is saved for next game.
+Who will be the best?
+
+Try it!
+
+GAME:
+Instructions:
+
+Title:
+Current time, Balls left, best time
+
+Controls:
+Arrow UP : Start/Accelerate
+Arrow Down: Back
+Arrow Left/Right: Direction of car
+B : Hand Break
+
+Keys:
+
+R: Restar game
+
+FOR DEVELOPER:
+F1: Debug mode
+F2: God mode
+C: Create a ball
+P:Change the cam between car cam and control cam with ASDW UP/DOWN:QE
+
+Link to GitHub: https://github.com/Pau5erra/RaceGame2.0
+
+Develop by Pau Serra & Guillem Muñoz
+Students of Universitat Politècnica de Catalunya Centre de la Imatge i Tecnologia Multimèdia
+Course 2015-2016 
