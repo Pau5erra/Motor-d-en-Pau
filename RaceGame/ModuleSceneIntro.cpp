40b86de2347785621f7e38c63cc9574b0e0a449c
Pau5erra <pau.serra11@hotmail.com>
2016-10-05 13:39:14 +0200

    Revert "imgui que encara no funciona framerate"
    This reverts commit 6b499b94f28374e2bf3f5eae4c432616c4596ffd.

--- RaceGame/ModuleSceneIntro.cpp
+++ RaceGame/ModuleSceneIntro.cpp
@@ -7,7 +7,6 @@
 #include "ImGui\imgui.h"
 #include "ImGui\imgui_impl_sdl.h"
 #include <ctime>
-#include <gl/GL.h>
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -25,7 +24,6 @@ bool ModuleSceneIntro::Start()
 
 	quit = false;
 	show_test_window = false;
-
 
 	return ret;
 }
@@ -57,44 +55,6 @@ update_status ModuleSceneIntro::Update(float dt)
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		ImGui::ShowTestWindow(&show_test_window);
 	}
-
-	char title[25];
-	sprintf_s(title, 25, "framerate%.1f", fps_log[fps_log.size() - 1]);
-	ImGui::PlotHistogram("##framerate", &fps_log[0], fps_log.size(), 0, title, 0.0f, 100.0f, ImVec2(310, 100));
-	sprintf_s(title, 25, "Milliseconds %0.1f", ms_log[ms_log.size() - 1]);
-	ImGui::PlotHistogram("##milliseconds", &ms_log[0], ms_log.size(), 0, title, 0.0f, 40.0f, ImVec2(310, 100));
-	
-	/*
-	glBegin(GL_TRIANGLES);
-	// front face =================
-	glVertex3fv(v0);    // v0-v1-v2
-	glVertex3fv(v1);
-	glVertex3fv(v2);
-
-	glVertex3fv(v2);    // v2-v3-v0
-	glVertex3fv(v3);
-	glVertex3fv(v0);
-
-	// right face =================
-	glVertex3fv(v0);    // v0-v3-v4
-	glVertex3fv(v3);
-	glVertex3fv(v4);
-
-	glVertex3fv(v4);    // v4-v5-v0
-	glVertex3fv(v5);
-	glVertex3fv(v0);
-
-	// top face ===================
-	glVertex3fv(v0);    // v0-v5-v6
-	glVertex3fv(v5);
-	glVertex3fv(v6);
-
-	glVertex3fv(v6);    // v6-v1-v0
-	glVertex3fv(v1);
-	glVertex3fv(v0);    // draw other 3 faces
-
-		glEnd();
-*/
 
 	return UPDATE_CONTINUE;
 }
